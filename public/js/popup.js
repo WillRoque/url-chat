5fad187751993d1f71f145f26bb7a9c27f7056cb
Willian Roque <willianfernandoroque@gmail.com>
2016-12-07 23:17:43 -0200

    Added some css and broadcast message in the back-end

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,5 +1,6 @@
 const socketIO = io('http://localhost:3000/');
 var tabUrl;
+var lastSender;
 
 /**
  * Joins the chat room of the URL of the current tab
@@ -46,8 +47,11 @@ function sendMessage() {
 
   console.log('sending message: ' + message);
 
+  addMessage(message, 'myself');
+
   socketIO.emit('chatMessage', {
     room: tabUrl,
+    sender: 'Sender Name',
     message: message
   });
 
@@ -57,12 +61,49 @@ function sendMessage() {
 /**
  * Receives a chat message from the server
  */
-socketIO.on('chatMessage', function (msg) {
-  console.log('receiving message: ' + msg);
-  var li = document.createElement('li');
-  li.appendChild(document.createTextNode(msg));
-  document.getElementById('messages').appendChild(li);
+socketIO.on('chatMessage', function (data) {
+  console.log('receiving message: ' + data);
+
+  addMessage(data.message, data.sender);
 });
+
+/**
+ * Adds a message to the chat, either it's been received or sent
+ * 
+ * @param message - message received/sent
+ * @param sender - if the message was sent by this client,
+ *   adds the sender's name on top of the message
+ */
+function addMessage(message, sender) {
+  var li = document.createElement('li');
+
+  if (lastSender && lastSender === sender) {
+    li.classList.add('continuous-message');
+  }
+
+  lastSender = sender;
+
+  var messageWrapper = document.createElement('div');
+  messageWrapper.className = 'message-wrapper';
+
+  if (sender && sender !== 'myself') {
+    var messageSender = document.createElement('div');
+    messageSender.className = 'message-sender';
+    messageSender.innerHTML = sender + ':';
+    messageWrapper.appendChild(messageSender);
+  } else {
+    messageWrapper.classList.add('my-message');
+  }
+
+  var messageDiv = document.createElement('div');
+  messageDiv.className = 'message';
+  messageDiv.appendChild(document.createTextNode(message));
+  messageWrapper.appendChild(messageDiv);
+
+  li.appendChild(messageWrapper);
+
+  document.getElementById('messages').appendChild(li);
+}
 
 /**
  * Sends a chat message to the server when the user
--- server/index.js
+++ server/index.js
@@ -13,19 +13,19 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('user connected');
 
     socket.on('joinRoom', (room) => {
-        console.log('socket joining room ' + room);
+        console.log('user joining room ' + room);
         socket.join(room);
     });
 
     socket.on('chatMessage', (data) => {
         console.log('received message: ' + data.message);
-        io.to(data.room).emit('chatMessage', data.message);
+        socket.broadcast.to(data.room).emit('chatMessage', { sender: data.sender, message: data.message });
     });
 
     socket.on('disconnect', () => console.log('user disconnected'));
 });
 
-httpServer.listen(3000);
+httpServer.listen(3000, () => console.log('url-chat running on port ' + 3000));
